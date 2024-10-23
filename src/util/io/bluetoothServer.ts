245b5b84de8d4c3c88e10b638ea15f6d8e5b5c99
1withspaghetti <79611123+1withspaghetti@users.noreply.github.com>
2024-10-23 10:38:03 -0700

    Bluetooth server reconnection

--- src/util/io/bluetoothServer.ts
+++ src/util/io/bluetoothServer.ts
@@ -6,6 +6,7 @@ const serviceUuid = parseInt('82480000-9a25-49fc-99be-2c16d1492d35');
 const txCharacteristicUuid = parseInt('82480001-9a25-49fc-99be-2c16d1492d35');
 const rxCharacteristicUuid = parseInt('82480002-9a25-49fc-99be-2c16d1492d35');
 
+let device: BluetoothDevice | null = null;
 let server: BluetoothRemoteGATTServer | null = null;
 
 let txCharacteristic: BluetoothRemoteGATTCharacteristic | null = null;
@@ -18,21 +19,33 @@ type EventMap = {
 }
 const events = new EventEmitter() as TypedEventEmitter<EventMap>;
 
+let isReconnecting = false;
+let reconnectAttemptsLeft = 0;
 
 // connect to the device, using cachedAsyncFunction to ensure that only one connection is attempted at a time
 const connect = cachedAsyncFunction(async () => {
-    if ('bluetooth' in navigator === false) throw new Error('Bluetooth not supported');
-    
-    const device = await navigator.bluetooth.requestDevice({
+    device = await navigator.bluetooth.requestDevice({
         filters: [{ services: [serviceUuid] }]
     });
+    await connectToDevice();
+});
+
+// This function being separate from connect() allows us to reconnect to the same device if it disconnects
+async function connectToDevice() {
+    if (device === null) throw new Error('No device selected');
 
     device.addEventListener('gattserverdisconnected', () => {
         console.log('Radio disconnected');
+        // Don't set device to null here, as we want to keep the device object around for reconnecting
         server = null;
         txCharacteristic = null;
         rxCharacteristic = null;
         events.emit('disconnected');
+
+        if (!isReconnecting) {
+            reconnectAttemptsLeft = 5;
+            tryReconnect();
+        }
     });
 
     server = (await device.gatt?.connect()) || null;
@@ -61,12 +74,30 @@ const connect = cachedAsyncFunction(async () => {
 
         events.emit('connected');
     } catch (e) {
+        device = null; // Clear device so we can't reconnect to it, as it doesn't have the correct characteristics
         server = null;
         txCharacteristic = null;
         rxCharacteristic = null;
         throw e;
     }
-});
+};
+
+async function tryReconnect() {
+    if (reconnectAttemptsLeft <= 0) {
+        isReconnecting = false;
+        return;
+    }
+    reconnectAttemptsLeft--;
+    isReconnecting = true;
+    
+    try {
+        console.log('Reconnecting to radio...');
+        await connectToDevice();
+    } catch (e) {
+        console.error('Failed to reconnect to radio:', e);
+        setTimeout(tryReconnect, 1000);
+    }
+}
 
 function isConnected() {
     return server !== null;
