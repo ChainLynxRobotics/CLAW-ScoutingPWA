b2034ad757ab81cce824b93f6a5e00a0c89e85ab
1withspaghetti <79611123+1withspaghetti@users.noreply.github.com>
2025-02-25 10:40:26 -0800

    Added descriptive statistics adding/subtracting

--- src/types/analyticsTypes.ts
+++ src/types/analyticsTypes.ts
@@ -1,10 +1,13 @@
-import { BlueAllianceAPIStatus, BlueAllianceMatch } from "./blueAllianceTypes";
-import { MatchData } from "./MatchData";
-
 export interface CustomTeamGroup {
     id: number;
     name: string;
     teams: number[];
+}
+
+export interface DescribedObjectStatistic<T> {
+    name: string;
+    path: Leaves<T>;
+    
 }
 
 // Defines a string union type for the different paths in an object
@@ -14,24 +17,51 @@ export type Leaves<T> = T extends object ? { [K in keyof T]:
     }[keyof T] : never
 
 export interface QuantitativeStats {
+    sample: Iterable<number>;
+    /** The smallest value within the data */
     min: number;
+    /** The largest value within the data */
     max: number;
+    /** The sum of all data added up */
     sum: number;
+    /** The number of data points (sample size) */
     n: number;
+    /** The average of all data points (mean) */
     avg: number;
+    /** The standard deviation of the data points */
     sd: number;
 }
 
+export interface QuantitativeProportionalStats {
+    samples: Iterable<[number, number]>;
+    /** The number of successes, quantitatively described */
+    x: QuantitativeStats;
+    /** The sample sizes, quantitatively described */
+    n: QuantitativeStats;
+    /** The proportions from 0-1, quantitatively described */
+    p: QuantitativeStats;
+}
+
 export interface CategoricalStats<T> {
+    sample: Iterable<T>;
+    /** The number of times each category appears */
     sum: Map<T, number>;
+    /** The proportion of the total for all categories that appear, Category mapped to Proportion from 0-1 */
     p: Map<T, number>;
+    /** Lowest proportion category, [category, proportion] */
     pMin: [T|undefined, number];
+    /** Highest proportion category, [category, proportion] */
     pMax: [T|undefined, number];
+    /** The sample size */
     n: number;
 }
 
-export interface BooleanStats {
-    sum: number;
+export interface ProportionalStats {
+    sample: Iterable<boolean>;
+    /** The number of successes */
+    x: number;
+    /** The proportion of successes, from 0-1 */
     p: number;
+    /** The sample size */
     n: number;
 }
--- src/util/analytics/objectStatistics.ts
+++ src/util/analytics/objectStatistics.ts
@@ -1,21 +1,4 @@
-import { BooleanStats, CategoricalStats, Leaves, QuantitativeStats } from "../../types/analyticsTypes";
-
-/**
- * Takes all the objects in objs and calculates the min, max, sum, n, and avg of the property at the defined path.
- * 
- * @param path A string representing the path to the property to analyze, e.g. 'alliances.blue.score', must be a number
- * @param objs A list of objects to analyze
- * @returns 
- */
-export function getQuantitativePropertyStats<T extends object>(path: Leaves<T>, objs: Iterable<T>): QuantitativeStats {
-    let sample = new Set<number>();
-    for (const obj of objs) {
-        let value = path.split('.').reduce((acc: any, key) => key in acc ? acc[key] : undefined, obj);
-        if (typeof value !== 'number') throw new Error(`Property ${path} is not a number`); // Unknown type
-        sample.add(value);
-    }
-    return getQuantitativeStats(sample);
-};
+import { ProportionalStats, CategoricalStats, Leaves, QuantitativeStats, QuantitativeProportionalStats } from "../../types/analyticsTypes";
 
 /**
  * Describes for a quantitative sample
@@ -23,7 +6,7 @@ export function getQuantitativePropertyStats<T extends object>(path: Leaves<T>, 
  * @param sample - List of numbers to analyze
  * @returns The min, max, sum, n, avg, and standard deviation of the sample
  */
-export function getQuantitativeStats(sample: Iterable<number>): QuantitativeStats {
+export function describeQuantitativeData(sample: Iterable<number>): QuantitativeStats {
     let min = Infinity;
     let max = -Infinity;
     let sum = 0;
@@ -36,6 +19,7 @@ export function getQuantitativeStats(sample: Iterable<number>): QuantitativeStat
         sum += obj;
         n++;
     }
+    if (n === 0) return { sample, min: NaN, max: NaN, sum: NaN, n: 0, avg: NaN, sd: NaN };
 
     const avg = sum / n;
 
@@ -46,37 +30,148 @@ export function getQuantitativeStats(sample: Iterable<number>): QuantitativeStat
     }
     const sd = Math.sqrt(sd_sum / (n - 1));
 
-    return { min, max, sum, n, avg, sd };
+    return { sample, min, max, sum, n, avg, sd };
 }
 
 /**
- * Takes all the objects in objs and calculates the sum, p, pMin, pMax, and n of the property at the defined path.
+ * Combines multiple samples of quantitative data and returns a single sample with the combined stats
  * 
- * @param path A string representing the path to the property to analyze, e.g. 'alliances.blue.score'
- * @param objs A list of objects to analyze
- * @returns 
+ * @param data - List of QuantitativeStats to combine
+ * @returns Resulting QuantitativeStats of the combined data
  */
-export function getCategoricalPropertyStats<T>(path: string, objs: Iterable<T>): CategoricalStats<T> {
-    let sample = new Set<T>();
-    for (const obj of objs) {
-        let value = path.split('.').reduce((acc: any, key) => key in acc ? acc[key] : undefined, obj);
-        if (typeof value === 'undefined' || typeof value === 'function' || typeof value === 'object' || typeof value === 'symbol') throw new Error(`Property ${path} cannot have type ${typeof value}`); // Unknown type
-        sample.add(value);
+export function addQuantitativeData(...data: QuantitativeStats[]): QuantitativeStats {
+    if (data.length === 0) return describeQuantitativeData([]);
+    if (data.length === 1) return data[0];
+    
+    const avg = data.reduce((acc, val) => acc + val.sum, 0) / data.reduce((acc, val) => acc + val.n, 0);
+
+    // We only need to fully recalculate the standard deviation, as the other stats can be calculated from the input stats
+    let combined_sample: number[] = [];
+    let sd_sum = 0;
+    for (const stat of data) {
+        combined_sample.push(...stat.sample);
+        for (const obj of stat.sample) {
+            const diff = obj - avg;
+            sd_sum += diff * diff;
+        }
     }
-    return getCategoricalStats(path, sample);
+    const sd = Math.sqrt(sd_sum / (data.reduce((acc, val) => acc + val.n, 0) - 1));
+
+    return {
+        sample: combined_sample,
+        min: Math.min(...data.map(stat => stat.min)),
+        max: Math.max(...data.map(stat => stat.max)),
+        sum: data.reduce((acc, val) => acc + val.sum, 0),
+        n: data.reduce((acc, val) => acc + val.n, 0),
+        avg,
+        sd
+    };
+}
+
+/**
+ * Subtracts two sets of quantitative data, to describe the difference in the data
+ * 
+ * Standard deviation is not calculated, as it is not useful in this context
+ * 
+ * Calculations are done as if the two sets of data were independent
+ * 
+ * @param a - First set of data
+ * @param b - Second set of data, to be subtracted from the first
+ * @returns Resulting QuantitativeStats of the subtracted data
+ */
+export function subtractQuantitativeData(a: QuantitativeStats, b: QuantitativeStats): QuantitativeStats {
+    return {
+        sample: [...a.sample, ...b.sample],
+        min: Math.min(a.min, b.min),
+        max: Math.max(a.max, b.max),
+        sum: a.sum - b.sum,
+        n: a.n - b.n,
+        avg: a.avg - b.avg,
+        sd: NaN
+    };
+}
+
+/**
+ * Describes multiple samples of success/fail data
+ * 
+ * Internally uses {@link describeQuantitativeData} on the x, n, and p data for each sample
+ * 
+ * @param samples - List of [successes, total] to analyze
+ * @returns The x, n and p of each sample quantitatively described
+ */
+export function describeQuantitativeProportionalData(samples: Iterable<[number, number]>): QuantitativeProportionalStats {
+    let x_list: number[] = []; // Successes
+    let p_list: number[] = []; // Proportions
+    let n_list: number[] = []; // Sample sizes
+
+    for (const [x, n] of samples) {
+        x_list.push(x);
+        p_list.push(x / n);
+        n_list.push(n);
+    }
+
+    return {
+        samples: samples,
+        x: describeQuantitativeData(x_list),
+        n: describeQuantitativeData(n_list),
+        p: describeQuantitativeData(p_list)
+    };
+}
+
+/**
+ * Combines multiple samples of success/fail data and returns a single sample with the combined stats
+ * 
+ * Internally uses {@link addQuantitativeData} to combine the x, n, and p data
+ * 
+ * @param data - List of QuantitativeProportionalStats to combine
+ * @returns Resulting QuantitativeProportionalStats of the combined data
+ */
+export function addQuantitativeProportionalData(...data: QuantitativeProportionalStats[]): QuantitativeProportionalStats {
+    if (data.length === 0) return { samples: [], x: addQuantitativeData(), n: addQuantitativeData(), p: addQuantitativeData() };
+    if (data.length === 1) return data[0];
+
+    let combined_samples: [number, number][] = [];
+    for (const stat of data) {
+        combined_samples.push(...stat.samples);
+    }
+
+    return {
+        samples: combined_samples,
+        x: addQuantitativeData(...data.map(stat => stat.x)),
+        n: addQuantitativeData(...data.map(stat => stat.n)),
+        p: addQuantitativeData(...data.map(stat => stat.p))
+    };
+}
+
+/**
+ * Subtracts two sets of success/fail data, to describe the difference in the data
+ * 
+ * Internally uses {@link subtractQuantitativeData} to subtract the x, n, and p data
+ * 
+ * @param a - First set of data
+ * @param b - Second set of data, to be subtracted from the first
+ * @returns Resulting QuantitativeProportionalStats of the subtracted data
+ */
+export function subtractQuantitativeProportionalData(a: QuantitativeProportionalStats, b: QuantitativeProportionalStats): QuantitativeProportionalStats {
+    return {
+        samples: [...a.samples, ...b.samples],
+        x: subtractQuantitativeData(a.x, b.x),
+        n: subtractQuantitativeData(a.n, b.n),
+        p: subtractQuantitativeData(a.p, b.p)
+    };
 }
 
 /**
  * Describes for a categorical sample
  * 
- * @param sample - List of categories to analyze
+ * @param sample - List of categories to analyze, suggested to be an enum or string
  * @returns The sum of each, p of each, pMin, pMax, and n of the sample
  */
-export function getCategoricalStats<T>(path: string, objs: Iterable<T>): CategoricalStats<T> {
+export function describeCategoricalData<T>(sample: Iterable<T>): CategoricalStats<T> {
     let sum = new Map<T, number>();
     let n = 0;
 
-    for (const obj of objs) {
+    for (const obj of sample) {
         // Update stats
         if (!sum.has(obj)) sum.set(obj, 0);
         sum.set(obj, sum.get(obj)! + 1);
@@ -93,25 +188,7 @@ export function getCategoricalStats<T>(path: string, objs: Iterable<T>): Categor
         if (value / n > pMax[1]) pMax = [key, value / n];
     }
 
-    return { sum, p, pMin, pMax, n };
-}
-
-
-/**
- * Takes all the objects in objs and calculates the sum, sumInverse, p, and n of the property at the defined path.
- * 
- * @param path A string representing the path to the property to analyze, e.g. 'alliances.blue.score'
- * @param objs A list of objects to analyze
- * @returns 
- */
-export function getBooleanPropertyStats(path: string, objs: Iterable<any>): BooleanStats {
-    let sample = new Set<boolean>();
-    for (const obj of objs) {
-        let value = path.split('.').reduce((acc: any, key) => key in acc ? acc[key] : undefined, obj);
-        if (typeof value !== 'boolean') throw new Error(`Property ${path} is not a boolean`); // Unknown type
-        sample.add(value);
-    }
-    return getBooleanStats(sample);
+    return { sample, sum, p, pMin, pMax, n };
 }
     
 
@@ -121,15 +198,15 @@ export function getBooleanPropertyStats(path: string, objs: Iterable<any>): Bool
  * @param sample - List of booleans to analyze
  * @returns The sum, sumInverse, p, and n of the sample
  */
-export function getBooleanStats(sample: Iterable<boolean>): BooleanStats {
-    let sum = 0;
+export function describeProportionalData(sample: Iterable<boolean>): ProportionalStats {
+    let x = 0;
     let n = 0;
 
     for (const obj of sample) {
         // Update stats
-        if (obj) sum++;
+        if (obj) x++;
         n++;
     }
 
-    return { sum, p: sum / n, n };
+    return { sample, x, p: x / n, n };
 }
