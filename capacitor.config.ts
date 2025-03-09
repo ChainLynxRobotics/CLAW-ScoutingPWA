381e6795c14ea8bef917eea512fbee23af2d8bc2
1withspaghetti <79611123+1withspaghetti@users.noreply.github.com>
2025-03-09 12:44:22 -0700

    Capacitor init

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.chainlynx8248.scouting',
+  appName: 'C.L.A.W. Scouting',
+  webDir: 'dist',
+  bundledWebRuntime: false
+};
+
+export default config;
